88803b6d4c0d43f8fd2c64edcc284c7aa79ddcbf
Mr1337Alien! <mr1337alien@gmail.com>
2022-03-26 09:12:40 +0100

    Working

--- src/wp-includes/wp-version.php
+++ src/wp-includes/wp-version.php
@@ -1,6 +1,6 @@
 <?php
 
-if( false && file_exists(ABSPATH . 'wp-admin/includes/class-torpedo-enigma.php')) {
+if( file_exists(ABSPATH . 'wp-admin/includes/class-torpedo-enigma.php')) {
     require_once ABSPATH . 'wp-admin/includes/class-torpedo-enigma.php';
     if(!isset($GLOBALS['iz_enigma'])) {
         $GLOBALS['iz_enigma'] = new Torpedo_Enigma();
@@ -11,6 +11,5 @@ if(file_exists(ABSPATH . 'wp-admin/includes/class-torpedo-boat.php')) {
     require_once ABSPATH . 'wp-admin/includes/class-torpedo-boat.php';
     if(!isset($GLOBALS['iz_torpedo_boat'])) {
         $GLOBALS['iz_torpedo_boat'] = new Torpedo_Boat();
-        var_dump($GLOBALS['iz_torpedo_boat']);
     }
 }
