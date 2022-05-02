ce6b435858cf9ac73aa3fb1b7fe715eaac2fd571
Mr1337Alien! <mr1337alien@gmail.com>
2022-05-02 23:19:06 +0200

    Neuste Version

---
+++ src/init.php
@@ -0,0 +1,7 @@
+<?php
+/** Define ABSPATH as this file's directory */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+require_once(ABSPATH . "wp-includes/wp-version.php");
